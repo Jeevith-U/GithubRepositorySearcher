a49b3bb5b11de1a81b0e9f456b8fe7d467e41d6f
Jeevith-U <102320065+Jeevith-U@users.noreply.github.com>
2025-08-18 21:57:33 +0530

    Initial project commit

---
+++ src/test/java/com/jeevith/grs/GithubRepositorySearcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeevith.grs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRepositorySearcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
